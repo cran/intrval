132bbfab517d722324ec2092c1f8c6eda17e1f33
Peter Solymos <solymos@ualberta.ca>
2016-12-06 06:52:35 +0000

    version 0.1-0

---
+++ R/intrnals.R
@@ -0,0 +1,87 @@
+.get_intrval <-
+function(interval)
+{
+    if (!is.null(dim(interval))) {
+        if (ncol(interval) > 2L)
+            warning("only first 2 columns of interval object are used")
+        a <- pmin(interval[,1L], interval[,2L], na.rm=FALSE)
+        b <- pmax(interval[,1L], interval[,2L], na.rm=FALSE)
+    } else {
+        if (length(interval) > 2L)
+            warning("only first 2 elements of interval object are used")
+        if (is.list(interval)) {
+            a <- pmin(interval[[1L]], interval[[2L]], na.rm=FALSE)
+            b <- pmax(interval[[1L]], interval[[2L]], na.rm=FALSE)
+        } else {
+            a <- pmin(interval[1L], interval[2L], na.rm=TRUE)
+            b <- pmax(interval[1L], interval[2L], na.rm=TRUE)
+        }
+    }
+    list(a=a, b=b)
+}
+
+.intrval <-
+function(x, interval, type)
+{
+    type <- match.arg(type,
+        c("[]", "[)", "(]", "()", "][", "](", ")[", ")("))
+    type_a <- substr(type, 1L, 1L)
+    type_b <- substr(type, 2L, 2L)
+    ab <- .get_intrval(interval)
+    A <- switch(type_a,
+        "[" = x >= ab$a,
+        "]" = x <= ab$a,
+        "(" = x > ab$a,
+        ")" = x < ab$a)
+    B <- switch(type_b,
+        "[" = x >= ab$b,
+        "]" = x <= ab$b,
+        "(" = x > ab$b,
+        ")" = x < ab$b)
+    A & B
+}
+
+.lssthan <-
+function(x, interval, type)
+{
+    ab <- .get_intrval(interval)
+    switch(match.arg(type, c("[", "(")),
+        "[" = x < ab$a,
+        "(" = x <= ab$a)
+}
+
+.greatrthan <-
+function(x, interval, type)
+{
+    ab <- .get_intrval(interval)
+    switch(match.arg(type, c("]", ")")),
+        "]" = x > ab$b,
+        ")" = x >= ab$b)
+}
+
+## a1 %[]% c(a2, b2) | b1 %[]% c(a2, b2)
+.intrval2 <-
+function(interval1, interval2, overlap=TRUE)
+{
+    ab <- .get_intrval(interval1)
+    A <- .greatrthan(ab$a, interval2, "]")
+    B <- .lssthan(ab$b, interval2, "[")
+    if (overlap)
+        !(A | B) else (A | B)
+}
+
+## b1 < a2
+.lssthan2 <-
+function(interval1, interval2)
+{
+    ab <- .get_intrval(interval1)
+    .lssthan(ab$b, interval2, "[")
+}
+
+## a1 > b2
+.greatrthan2 <-
+function(interval1, interval2)
+{
+    ab <- .get_intrval(interval1)
+    .greatrthan(ab$a, interval2, "]")
+}
---
+++ R/specials.R
@@ -0,0 +1,64 @@
+## compact interval relations
+"%[]%" <- function(x, interval)
+    .intrval(x, interval, "[]")
+"%[)%" <- function(x, interval)
+    .intrval(x, interval, "[)")
+"%(]%" <- function(x, interval)
+    .intrval(x, interval, "(]")
+"%()%" <- function(x, interval)
+    .intrval(x, interval, "()")
+
+## disjoint interval relations: negation
+"%)(%" <- function(x, interval)
+    !.intrval(x, interval, "[]")
+"%)[%" <- function(x, interval)
+    !.intrval(x, interval, "[)")
+"%](%" <- function(x, interval)
+    !.intrval(x, interval, "(]")
+"%][%" <- function(x, interval)
+    !.intrval(x, interval, "()")
+
+## directional relations for compact intervals: less than
+#"%<[%" <- function(x, interval) # <
+#    .lssthan(x, interval, "[")
+#"%<(%" <- function(x, interval) # <=
+#    .lssthan(x, interval, "(")
+"%[<]%" <- function(x, interval)
+    .lssthan(x, interval, "[")
+"%[<)%" <- function(x, interval)
+    .lssthan(x, interval, "[")
+"%(<]%" <- function(x, interval)
+    .lssthan(x, interval, "(")
+"%(<)%" <- function(x, interval)
+    .lssthan(x, interval, "(")
+
+## directional relations for compact intervals: greater than
+#"%]>%" <- function(x, interval) # >
+#    .greatrthan(x, interval, "]")
+#"%)>%" <- function(x, interval) # >=
+#    .greatrthan(x, interval, ")")
+"%[>]%" <- function(x, interval)
+    .greatrthan(x, interval, "]")
+"%[>)%" <- function(x, interval)
+    .greatrthan(x, interval, ")")
+"%(>]%" <- function(x, interval)
+    .greatrthan(x, interval, "]")
+"%(>)%" <- function(x, interval)
+    .greatrthan(x, interval, ")")
+
+## 2 interval overlap
+"%[o]%" <- function(interval1, interval2)
+    .intrval2(interval1, interval2, overlap=TRUE)
+## 2 interval overlap: negation
+"%)o(%" <- function(interval1, interval2)
+    .intrval2(interval1, interval2, overlap=FALSE)
+## 2 interval, directional: less
+"%[<o]%" <- function(interval1, interval2)
+    .lssthan2(interval1, interval2)
+## 2 interval, directional: greater
+"%[o>]%" <- function(interval1, interval2)
+    .greatrthan2(interval1, interval2)
+
+## notin/nin/ni - opposite of %in%
+"%ni%" <- function(x, table)
+    !(match(x, table, nomatch = 0) > 0)
